3e97805075bfd1b0bc699e8402b458a89397e2c9
Retrooper <uchizi40000@gmail.com>
2020-07-06 13:57:32 +0800

    Updated example,
    * Updated to PacketEvents 1.4.2!
    * if else statements converted to a switch statement

--- src/main/java/io/github/retrooper/examplepacketevents/Main.java
+++ src/main/java/io/github/retrooper/examplepacketevents/Main.java
@@ -8,7 +8,6 @@ import io.github.retrooper.packetevents.event.PacketListener;
 import io.github.retrooper.packetevents.event.impl.PacketReceiveEvent;
 import io.github.retrooper.packetevents.event.impl.PacketSendEvent;
 import io.github.retrooper.packetevents.packet.PacketType;
-import io.github.retrooper.packetevents.packet.PacketTypeNames;
 import io.github.retrooper.packetevents.packetwrappers.in.chat.WrappedPacketInChat;
 import io.github.retrooper.packetevents.packetwrappers.in.flying.WrappedPacketInFlying;
 import io.github.retrooper.packetevents.packetwrappers.out.abilities.WrappedPacketOutAbilities;
@@ -28,7 +27,6 @@ public class Main extends JavaPlugin implements PacketListener {
     public void onEnable() {
         PacketEvents.start(this);
         PacketEvents.getAPI().getEventManager().registerListener(this);
-        PacketEvents.getAPI().getServerTickTask().cancel();
     }
 
     @Override
@@ -57,27 +55,29 @@ public class Main extends JavaPlugin implements PacketListener {
     public void onReceive(PacketReceiveEvent e) {
         if (e.getPacketId() == PacketType.Client.CHAT) {
             WrappedPacketInChat chat = new WrappedPacketInChat(e.getNMSPacket());
-            if (chat.getMessage().equals("pls jump")) {
-                WrappedPacketOutEntityVelocity velocity = new WrappedPacketOutEntityVelocity(e.getPlayer(), 0, 1, 0);
-                PacketEvents.getAPI().getPlayerUtilities().sendPacket(e.getPlayer(), velocity);
-            } else if (chat.getMessage().equals("pls swing")) {
-                WrappedPacketOutAnimation anim = new WrappedPacketOutAnimation(e.getPlayer(), EntityAnimationType.SWING_MAIN_ARM);
-                PacketEvents.getAPI().getPlayerUtilities().sendPacket(e.getPlayer(), anim);
-            }
-            else if(chat.getMessage().equals("what is my client version")) {
-                //ViaVersion or ProtocolSupport or ProtocolLib is required to do this
-                final ClientVersion clientVersion = PacketEvents.getAPI().getPlayerUtilities().getClientVersion(e.getPlayer());
-                if(clientVersion == ClientVersion.ACCESS_FAILURE) {
-                    //ViaVersion or ProtocolSupport or ProtocolLib could not be found
-                }
-                else {
-                    e.getPlayer().sendMessage("Your client version is " + clientVersion.name());
-                }
-            }
-            else if(chat.getMessage().equals("give me abilities")) {
-                //arguments: vulnerable, isFlying, allowFlight, canBuildInstantly, flySpeed, walkSpeed
-                WrappedPacketOutAbilities abilities = new WrappedPacketOutAbilities(true, false, true, false, 10, 2);
-                PacketEvents.getAPI().getPlayerUtilities().sendPacket(e.getPlayer(), abilities);
+            switch (chat.getMessage()) {
+                case "pls jump":
+                    WrappedPacketOutEntityVelocity velocity = new WrappedPacketOutEntityVelocity(e.getPlayer(), 0, 1, 0);
+                    PacketEvents.getAPI().getPlayerUtilities().sendPacket(e.getPlayer(), velocity);
+                    break;
+                case "pls swing":
+                    WrappedPacketOutAnimation anim = new WrappedPacketOutAnimation(e.getPlayer(), EntityAnimationType.SWING_MAIN_ARM);
+                    PacketEvents.getAPI().getPlayerUtilities().sendPacket(e.getPlayer(), anim);
+                    break;
+                case "what is my client version":
+                    //ViaVersion or ProtocolSupport or ProtocolLib is required to do this
+                    final ClientVersion clientVersion = PacketEvents.getAPI().getPlayerUtilities().getClientVersion(e.getPlayer());
+                    if (clientVersion == ClientVersion.ACCESS_FAILURE) {
+                        //ViaVersion or ProtocolSupport or ProtocolLib could not be found
+                    } else {
+                        e.getPlayer().sendMessage("Your client version is " + clientVersion.name());
+                    }
+                    break;
+                case "give me abilities":
+                    //arguments: vulnerable, isFlying, allowFlight, canBuildInstantly, flySpeed, walkSpeed
+                    WrappedPacketOutAbilities abilities = new WrappedPacketOutAbilities(true, false, true, false, 10, 2);
+                    PacketEvents.getAPI().getPlayerUtilities().sendPacket(e.getPlayer(), abilities);
+                    break;
             }
         }
         else if(PacketType.Client.Util.isInstanceOfFlying(e.getPacketId())) {
